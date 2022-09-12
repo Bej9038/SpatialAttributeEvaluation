64e4c0b4a62ead65f54ec82361b730c8b190057f
Ben Jordan <bej9038@rit.edu>
2022-09-11 21:34:40 -0400

    Added sphere visualization, put web gl compatability check into new service.

--- src/app/Components/view/view.component.ts
+++ src/app/Components/view/view.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, inject, ViewChild} from '@angular/core';
-import { ScriptService } from "../../Services/script.service";
+import {WebGlService} from "../../Services/web-gl.service";
 import * as THREE from "three";
+import { gsap } from "gsap";
 
 @Component({
   selector: 'view',
@@ -10,11 +11,11 @@ import * as THREE from "three";
 export class ViewComponent {
   // @ts-ignore
   @ViewChild('view') view: ElementRef<HTMLCanvasElement>;
-  private script:ScriptService = new ScriptService();
+  private webGl: WebGlService = new WebGlService();
 
   ngAfterViewInit()
   {
-    if(this.isWebGlAvailable())
+    if(this.webGl.isWebGlAvailable())
     {
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -22,36 +23,59 @@ export class ViewComponent {
         canvas: this.view.nativeElement
       });
       renderer.setSize( window.innerWidth, window.innerHeight );
-      const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      const cube = new THREE.Mesh( geometry, material );
-      scene.add( cube );
-      renderer.render(scene, camera);
 
-      this.script.load('filepicker', 'rangeSlider').then(data => {
-        console.log('script loaded ', data);
-      }).catch(error => console.log(error));
-    }
-  }
+      window.addEventListener("resize", function ()
+      {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+      })
 
+      let distance = Math.min(200, window.innerWidth / 4);
+      let geometry = new THREE.BufferGeometry();
+      let points = [];
 
-  isWebGlAvailable()
-  {
-    if (this.attemptWebGl()) {
-      return true;
-    } else {
-      //display error message
-      return false;
-    }
-  }
+      for(let i = 0; i < 1600; i++)
+      {
+        let vertex = new THREE.Vector3();
+        let theta = THREE.MathUtils.randFloatSpread(360);
+        let phi = THREE.MathUtils.randFloatSpread(360);
 
-  attemptWebGl()
-  {
-    try {
-      const canvas = document.createElement( 'canvas' );
-      return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) );
-    } catch ( e ) {
-      return false;
+        vertex.x = distance * Math.sin(theta) * Math.cos(phi);
+        vertex.y = distance * Math.sin(theta) * Math.sin(phi);
+        vertex.z = distance * Math.cos(theta);
+
+        points.push(vertex);
+      }
+      geometry.setFromPoints(points);
+      geometry.boundingSphere = new THREE.Sphere(new THREE.Vector3(0, 0, 0), 50);
+
+      let particles = new THREE.Points(geometry, new THREE.PointsMaterial(
+        {color: 0xff44ff, size: 2}
+      ));
+      let renderingParent = new THREE.Group();
+      renderingParent.add(particles);
+
+      let resizeContainer = new THREE.Group();
+      resizeContainer.add(renderingParent);
+      scene.add(resizeContainer);
+
+      camera.position.z = 400;
+
+      var animate = function ()
+      {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+      }
+
+      animate();
+      let animProps = {scale: 1, xRot: 0, yRot: 0};
+      gsap.to(animProps, {duration: 10, scale: 1.3, repeat: -1, yoyo:true, ease: "sine", onUpdate: function (){
+        renderingParent.scale.set(animProps.scale, animProps.scale, animProps.scale);
+        }});
+      gsap.to(animProps, {duration: 120, xRot: Math.PI * 2, yRot: Math.PI * 4, repeat: -1, yoyo:true, ease: "none", onUpdate: function (){
+          renderingParent.rotation.set(animProps.scale, animProps.scale, animProps.scale);
+        }})
     }
   }
 }
--- src/app/Services/script.store.ts
+++
@@ -1,7 +0,0 @@
-interface Scripts {
-  name: string;
-  src: string;
-}
-export const ScriptStore: Scripts[] = [
-  {name: 'gsap', src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.1/gsap.min.js'}
-];
