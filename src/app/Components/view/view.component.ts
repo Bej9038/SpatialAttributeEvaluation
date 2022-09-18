03fedcece06796540fed71d2df45e49434c21c07
Ben Jordan <bej9038@rit.edu>
2022-09-18 10:57:02 -0400

    Added no web GL warning

--- src/app/Components/view/view.component.ts
+++ src/app/Components/view/view.component.ts
@@ -15,28 +15,28 @@ export class ViewComponent {
 
   ngAfterViewInit()
   {
-    if(this.webGl.isWebGlAvailable())
-    {
+    if(!this.webGl.isWebGlAvailable()) {
+      window.alert("error, webGL not available");
+    }
+    else {
       const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
       const renderer = new THREE.WebGLRenderer({
         canvas: this.view.nativeElement
       });
-      renderer.setSize( window.innerWidth, window.innerHeight );
+      renderer.setSize(window.innerWidth, window.innerHeight);
 
-      window.addEventListener("resize", function ()
-      {
+      window.addEventListener("resize", function () {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
-        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.setSize(window.innerWidth, window.innerHeight);
       })
 
       let distance = Math.min(200, window.innerWidth / 4);
       let geometry = new THREE.BufferGeometry();
       let points = [];
 
-      for(let i = 0; i < 1600; i++)
-      {
+      for (let i = 0; i < 1600; i++) {
         let vertex = new THREE.Vector3();
         let theta = THREE.MathUtils.randFloatSpread(360);
         let phi = THREE.MathUtils.randFloatSpread(360);
@@ -62,20 +62,33 @@ export class ViewComponent {
 
       camera.position.z = 400;
 
-      var animate = function ()
-      {
+      let animate = function () {
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
       }
 
       animate();
+
       let animProps = {scale: 1, xRot: 0, yRot: 0};
-      gsap.to(animProps, {duration: 10, scale: 1.3, repeat: -1, yoyo:true, ease: "sine", onUpdate: function (){
-        renderingParent.scale.set(animProps.scale, animProps.scale, animProps.scale);
-        }});
-      gsap.to(animProps, {duration: 120, xRot: Math.PI * 2, yRot: Math.PI * 4, repeat: -1, yoyo:true, ease: "none", onUpdate: function (){
+
+      gsap.to(animProps, {
+        duration: 10, scale: 1.3, repeat: -1, yoyo: true, ease: "sine", onUpdate: function () {
+          renderingParent.scale.set(animProps.scale, animProps.scale, animProps.scale);
+        }
+      });
+
+      gsap.to(animProps, {
+        duration: 120,
+        xRot: Math.PI * 2,
+        yRot: Math.PI * 4,
+        repeat: -1,
+        yoyo: true,
+        ease: "none",
+        onUpdate: function () {
           renderingParent.rotation.set(animProps.scale, animProps.scale, animProps.scale);
-        }})
+        }
+      });
+
     }
   }
 }
