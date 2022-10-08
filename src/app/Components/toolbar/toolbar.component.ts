771482e32b443bbe245cf6892139a3c8110e4af0
Ben Jordan <bej9038@rit.edu>
2022-10-08 19:25:07 -0400

    Adjusted main grid list sizing in preparation for playback controls being added. Added logo.

---
+++ src/app/Components/toolbar/toolbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {ResetWarning} from "../reset-warning/reset-warning.component";
+import {MatDialog} from "@angular/material/dialog";
+import {WelcomeMenu} from "../welcome-menu/welcome-menu.component";
+
+@Component({
+  selector: 'toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class Toolbar{
+
+  constructor(public dialog: MatDialog) {}
+
+  endSession()
+  {
+    this.dialog.open(ResetWarning,
+      {
+        exitAnimationDuration: '.4s',
+        enterAnimationDuration: '.4s',
+        height: '160px',
+        autoFocus: false
+      });
+  }
+}
