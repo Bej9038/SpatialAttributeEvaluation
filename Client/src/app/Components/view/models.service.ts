e9f698863ad71ad16a26a12106ac159b6ae4b99f
Ben Jordan <bej9038@rit.edu>
2023-01-11 11:06:13 -0500

    Moved sphere to the models service as well.

--- Client/src/app/Components/view/models.service.ts
+++ Client/src/app/Components/view/models.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 import * as THREE from "three";
+import {ShadersService} from "./shaders.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModelsService {
+
+  // @ts-ignore
+  sphereGeometry: THREE.SphereGeometry;
+  // @ts-ignore
+  sphereMaterial: THREE.ShaderMaterial;
+  // @ts-ignore
+  sphereObject: THREE.Mesh;
+
+  offsetSphr: THREE.Spherical;
+  offsetDir: THREE.Vector3;
+  sphereSubdivs: number = 1024;
+  sphereRad: number = 1.0;
 
   // @ts-ignore
   immersionGeometry: THREE.BufferGeometry;
@@ -14,16 +27,84 @@ export class ModelsService {
   immersionObject: THREE.Points;
   // @ts-ignore
   immersionPositionArr: Float32Array;
-
   numImmersionParticles:number = 10000;
 
-  constructor() {
+  sphereClarity:number = 0.0;
+  sphereWidth:number = 1.0;
+  sphereDepth:number = 1.0;
+  sphereImmersion:number = 0.0;
+
+  constructor(private shaderStore: ShadersService) {
     this.immersionPositionArr = new Float32Array(this.numImmersionParticles * 3);
+    this.offsetSphr = new THREE.Spherical(1, Math.random() * Math.PI, Math.random() * Math.PI * 2);
+    this.offsetDir = new THREE.Vector3();
+    this.offsetDir.setFromSpherical(this.offsetSphr);
+  }
+
+  getSphereObject()
+  {
+    return this.sphereObject;
+  }
+
+  generateSphere()
+  {
+    this.initSphereGeometry();
+    this.initSphereMaterial();
+    this.sphereObject = new THREE.Mesh(this.sphereGeometry, this.sphereMaterial);
+  }
+
+  initSphereGeometry()
+  {
+    this.sphereGeometry = new THREE.SphereGeometry(this.sphereRad, this.sphereSubdivs, this.sphereSubdivs);
+    this.sphereGeometry.computeTangents();
+  }
+
+  initSphereMaterial()
+  {
+    let uniforms = THREE.UniformsUtils.merge([
+      THREE.UniformsLib["fog"],
+      THREE.UniformsLib[ "lights" ],
+      {
+        //baseTexture: { value: null },
+        //bloomTexture: { value: this.bloomComposer.renderTarget2.texture },
+        uPerimeter: {value: this.sphereGeometry.parameters.radius},
+        uSubDivisions: {value: this.sphereGeometry.parameters.heightSegments},
+        uTime: { value: 0.0 },
+        uWidth: { value: 1.0 },
+        uDepth: { value: 1.0 },
+        uOffsetDirection: { value: this.offsetDir },
+        uOffsetSpeed: { value: 1.6 },
+        uDistortionFrequency: { value: 2.0},
+        uDistortionStrength: { value: 1.0},
+        uDisplacementStrength: { value: 0.0 },
+        uDisplacementFrequency: { value: 2.0},
+        uAverageFrequency: { value: 1.0 },
+      }
+    ]);
+
+    this.sphereMaterial = new THREE.ShaderMaterial({
+      lights:true,
+      fog:true,
+      uniforms: uniforms,
+      defines: {
+        USE_TANGENT: ''
+      },
+      vertexShader: this.shaderStore.vertexShader,
+      fragmentShader: this.shaderStore.fragmentShader,
+    });
   }
 
   getImmersionObject()
   {
     return this.immersionObject;
+  }
+
+  updateImmersion()
+  {
+    this.immersionObject.geometry.setAttribute('position',
+      new THREE.BufferAttribute(this.immersionPositionArr.slice(0, this.sphereImmersion * 100 * 3), 3));
+    this.immersionObject.rotateY(0.0002);
+    this.immersionObject.rotateX(0.00015);
   }
 
   generateImmersion()
--- Client/src/app/Components/view/view.component.ts
+++ Client/src/app/Components/view/view.component.ts
@@ -2,7 +2,6 @@ import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import {WebGlService} from "../../Services/web-gl.service";
 import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import {ShadersService} from "./shaders.service";
 import {SessionValuesService} from "../../Services/session-values.service";
 import {AudioService} from "../../Services/audio.service";
 import {EffectComposer} from "three/examples/jsm/postprocessing/EffectComposer";
@@ -31,36 +30,23 @@ export class ViewComponent {
   camera: THREE.PerspectiveCamera;
   time: THREE.Clock;
   bloomLayer: THREE.Layers;
-  // @ts-ignore
-  sphereGeometry: THREE.SphereGeometry;
-  // @ts-ignore
-  sphereMaterial: THREE.ShaderMaterial;
-  // @ts-ignore
-  sphereObject: THREE.Mesh;
 
-  offsetSphr: THREE.Spherical;
-  offsetDir: THREE.Vector3;
-  sphereSubdivs: number = 1024;
-  sphereRad: number = 1.0;
   sphereClarity:number = 0.0;
   sphereWidth:number = 1.0;
   sphereDepth:number = 1.0;
   sphereImmersion:number = 0.0;
 
-  constructor(public models:ModelsService, public audio:AudioService, private sessionValues: SessionValuesService, private webGl: WebGlService, private shaderStore: ShadersService) {
+  constructor(public models:ModelsService, public audio:AudioService, private sessionValues: SessionValuesService, private webGl: WebGlService) {
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderScene = new RenderPass(this.scene, this.camera);
     this.time = new THREE.Clock();
     this.bloomLayer = new THREE.Layers();
-    this.offsetSphr = new THREE.Spherical(1, Math.random() * Math.PI, Math.random() * Math.PI * 2);
-    this.offsetDir = new THREE.Vector3();
   }
 
   ngOnInit() {
     THREE.Cache.enabled = true;
     this.time.start();
-    this.offsetDir.setFromSpherical(this.offsetSphr);
     this.bloomLayer.set(1);
 
     this.sessionValues.clarity.subscribe(clarity => {
@@ -92,11 +78,8 @@ export class ViewComponent {
       this.models.generateImmersion();
       this.scene.add(this.models.getImmersionObject());
 
-      this.sphereGeometry = this.initSphereGeometry();
-      this.sphereMaterial = this.initSphereMaterial();
-      this.sphereObject = this.generateSphere();
-
-      this.scene.add(this.sphereObject);
+      this.models.generateSphere();
+      this.scene.add(this.models.getSphereObject());
 
       //this.scene.fog = new THREE.FogExp2(0x11111f, 0.25);
       //this.scene.add( new THREE.AmbientLight( 0x404040 ) );
@@ -111,60 +94,6 @@ export class ViewComponent {
       }
       animate();
     }
-  }
-
-  // create objects
-
-
-
-  generateSphere()
-  {
-    let mesh = new THREE.Mesh(this.sphereGeometry, this.sphereMaterial);
-    return mesh;
-  }
-
-  initSphereGeometry()
-  {
-    let geometry = new THREE.SphereGeometry(this.sphereRad, this.sphereSubdivs, this.sphereSubdivs);
-    geometry.computeTangents();
-    return geometry;
-  }
-
-  initSphereMaterial()
-  {
-    let uniforms = THREE.UniformsUtils.merge([
-      THREE.UniformsLib["fog"],
-      THREE.UniformsLib[ "lights" ],
-      {
-        baseTexture: { value: null },
-        bloomTexture: { value: this.bloomComposer.renderTarget2.texture },
-        uPerimeter: {value: this.sphereGeometry.parameters.radius},
-        uSubDivisions: {value: this.sphereGeometry.parameters.heightSegments},
-        uTime: { value: 0.0 },
-        uWidth: { value: 1.0 },
-        uDepth: { value: 1.0 },
-        uOffsetDirection: { value: this.offsetDir },
-        uOffsetSpeed: { value: 1.6 },
-        uDistortionFrequency: { value: 2.0},
-        uDistortionStrength: { value: 1.0},
-        uDisplacementStrength: { value: 0.0 },
-        uDisplacementFrequency: { value: 2.0},
-        uAverageFrequency: { value: 1.0 },
-      }
-    ]);
-
-    let sphereMaterial = new THREE.ShaderMaterial({
-      lights:true,
-      fog:true,
-      uniforms: uniforms,
-      defines: {
-        USE_TANGENT: ''
-      },
-      vertexShader: this.shaderStore.vertexShader,
-      fragmentShader: this.shaderStore.fragmentShader,
-    });
-
-    return sphereMaterial;
   }
 
   // utility functions
@@ -190,7 +119,7 @@ export class ViewComponent {
     this.bloomComposer.addPass(this.renderScene);
     this.bloomComposer.addPass(bloom);
 
-    let finalpass = new ShaderPass(this.sphereMaterial, 'baseTexture');
+    let finalpass = new ShaderPass(this.models.sphereMaterial, 'baseTexture');
     finalpass.needsSwap = true;
     this.finalComposer.addPass(this.renderScene);
     this.finalComposer.addPass(finalpass);
@@ -213,16 +142,16 @@ export class ViewComponent {
     this.models.getImmersionObject().rotateY(0.0002);
     this.models.getImmersionObject().rotateX(0.00015);
 
-    this.sphereMaterial.uniforms['uTime'].value = this.time.getElapsedTime() * 0.8;
-    this.sphereMaterial.uniforms['uDisplacementStrength'].value = this.sphereClarity + this.audio.analyzerLevel;
-    this.sphereMaterial.uniforms['uWidth'].value = this.sphereWidth;
-    this.sphereMaterial.uniforms['uDepth'].value = this.sphereDepth;
-    this.sphereMaterial.uniforms['uAverageFrequency'].value = this.audio.analyser.getAverageFrequency();
+    this.models.sphereMaterial.uniforms['uTime'].value = this.time.getElapsedTime() * 0.8;
+    this.models.sphereMaterial.uniforms['uDisplacementStrength'].value = this.sphereClarity + this.audio.analyzerLevel;
+    this.models.sphereMaterial.uniforms['uWidth'].value = this.sphereWidth;
+    this.models.sphereMaterial.uniforms['uDepth'].value = this.sphereDepth;
+    this.models.sphereMaterial.uniforms['uAverageFrequency'].value = this.audio.analyser.getAverageFrequency();
 
     let t = this.time.getElapsedTime() * 25;
-    this.offsetSphr.phi = ((Math.sin(t * 0.0056) * Math.sin(t * 0.0048)) * 0.5 + 0.5) * Math.PI;
-    this.offsetSphr.theta = ((Math.sin(t * 0.0024) * Math.sin(t * 0.00152)) * 0.5 + 0.5) * Math.PI * 2;
-    this.offsetDir.setFromSpherical(this.offsetSphr);
+    this.models.offsetSphr.phi = ((Math.sin(t * 0.0056) * Math.sin(t * 0.0048)) * 0.5 + 0.5) * Math.PI;
+    this.models.offsetSphr.theta = ((Math.sin(t * 0.0024) * Math.sin(t * 0.00152)) * 0.5 + 0.5) * Math.PI * 2;
+    this.models.offsetDir.setFromSpherical(this.models.offsetSphr);
   }
 
   @HostListener('window:resize', ['$event'])
